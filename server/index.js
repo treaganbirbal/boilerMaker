9b677c5ef9a0c7351203933b9bc151d6fa05daec
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-05 21:18:35 -0400

    setup express server

--- server/index.js
+++ server/index.js
@@ -4,3 +4,27 @@ const app = express();
 const morgan = require('morgan')
 
 app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../client')))
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/api', require('./routes'))
+
+app.get('*', function (req, res){
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Your server is listening on port ${port}`)
+})
