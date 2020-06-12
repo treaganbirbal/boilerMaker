e5888a7d5b5624744596c43d6cb51e4c04f1ba4f
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-11 21:58:42 -0400

    action types, action create, thunks, reducer

--- client/store/user.js
+++ client/store/user.js
@@ -1,11 +1,63 @@
-//action type
+import axios from 'axios'
 
-//action create
+
+//action types
+const GET_USER = 'GET_USER'
+const REMOVE_USER = 'REMOVE_USER'
+
+//action creates
+const getUser = user =>({type: GET_USER, user})
+const removeUser = () => ({type: REMOVE_USER})
+
+//thunk creators
+
+export const me = async (disatch) => {
+    try {
+        const res = await axios.get('/auth/me')
+        dispatchEvent(getUser(res.data || defaultUser))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const auth = (email, password, method) => {
+    async dispatch => {
+        let res
+        try {
+            res = await axios.post(`/auth/${method}`, {email, password})
+        } catch (authError) {
+            return dispatch(getUser({error: authError}))
+        }
+        try {
+           dispatch(getUser(res.data))
+           history.push('/home')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export const logout = () => async dispatch => {
+    try {
+        await axios.post('/auth/logout')
+        dispatch(removeUser())
+        history.push('/login')
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 //state
-const dummyState = {}
+const defaultUser = {}
 
 //reducer
-const dummyReducer = (state=dummyState, action) => {
-    return state
+const dummyReducer = (state=defaultUser, action) => {
+    switch (action.type){
+        case GET_USER:
+            return action.user;
+        case REMOVE_USER:
+            return defaultUser;
+        default:
+            return state
+    }
 }
