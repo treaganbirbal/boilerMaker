54a7a760c2eca2d208538fe91f7212235dae03b8
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-12 17:58:23 -0400

    encrypt user passwords

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,5 +1,6 @@
  const Sequelize = require('sequelize')
 const db = require('../db')
+const crypto = require('crypto')
 
 const User = db.define('user', {
     email: {
@@ -23,5 +24,33 @@ const User = db.define('user', {
         type: Sequelize.STRING
     }
 })
+//instanceMethods 
+User.prototype.correctPassword = function(candidatePwd){
+    return User.encryptPassword(candidatePwd, this.salt() === this.password)
+}
+
+//classMethods
+User.generateSalt = function(){
+    return crypto.randomBytes(16).toString('base64')
+}
+
+User.encryptPassword = function(plainText, salt){
+    return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex')
+}
+
+//hooks
+const setSaltAndPassword = user => {
+    if(user.changed('password')){
+        user.salt = User.generateSalt()
+        user.password = User.encryptPassword(user.password(), user.salt())
+    }
+}
+
+User.beforeCreate(setSaltAndPassword)
+User.beforeUpdate(setSaltAndPassword)
 
 module.exports = User 
